7af5fef4312950568a54599184db895f4b729693
student <student@student03.local>
2019-04-25 21:48:00 -0300

    Inicializacao de Projeto

---
+++ TailorTeste/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TailorTeste
+//
+//  Created by Student on 24/04/2019.
+//  Copyright © 2019 Hackatruck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
